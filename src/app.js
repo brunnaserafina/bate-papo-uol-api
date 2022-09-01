3b844aeba13ed5ccedc3d974910ff578bc400ffd
Brunna Serafina <brunnaserafina@gmail.com>
2022-08-31 23:29:27 -0300

    build: adding dotenv and mongodb package

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect(() => {
+  db = mongoClient.db("test");
+});
 
 const server = express();
 server.use(cors());
