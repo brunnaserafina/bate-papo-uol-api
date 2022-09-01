6b5219cde1b178c829fb02e0981880e32187b53b
Brunna Serafina <brunnaserafina@gmail.com>
2022-09-01 11:47:32 -0300

    feat: preventing registration of username already used, saving participant and message in the database and returning status 201

--- src/app.js
+++ src/app.js
@@ -15,4 +15,39 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+server.post("/participants", async (req, res) => {
+  const { name } = req.body;
+  const hour = dayjs().format("HH:mm:ss");
+
+  try {
+    if (!name) {
+      return res.sendStatus(422);
+    }
+
+    const participantsCollect = db.collection("participants");
+    const containName = await participantsCollect.findOne({ name: name });
+
+    if (containName) {
+      console.log(containName);
+      return res.sendStatus(409);
+    }
+
+    const messagesCollect = db.collection("messages");
+
+    await participantsCollect.insertOne({ name, lastStatus: Date.now() });
+    await messagesCollect.insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: hour,
+    });
+
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
 server.listen(5000, () => console.log("Listening on port 5000"));
